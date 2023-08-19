1a79fed9c783c0a79310c138a3d917010f081a18
shahzaib <140626508+Shahzaibhunxai@users.noreply.github.com>
2023-08-19 13:04:37 +0500

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=1; i<=4; i++){
+        cout<<"week no#"<<i<<endl;
+        for(int x=1; x<=7; x++){
+            cout<<"day"<<x<<endl;
+        }
+    }
+}
